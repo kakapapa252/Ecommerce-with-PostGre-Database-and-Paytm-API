4d16087fc08df3b93e8439a6734438852805832d
kakapapa252 <kartikay252@gmail.com>
2021-02-01 20:07:00 +0530

    whitenoise in rqmnt.txt

--- Ecom/Ecom/settings.py
+++ Ecom/Ecom/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #static with heroku
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Ecom.urls'
